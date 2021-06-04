d4aa8b0c66087949fa9cd57fb55cdc5273dc6cf9
MadeByMe <77327581+testmadebyme@users.noreply.github.com>
2021-06-04 23:54:32 +0300

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,7 @@ AP.request('https://bymetest.atlassian.net/rest/api/3/issuetype', {
     success: function(responseText){
         var mydata = JSON.parse(responseText);
         console.log(mydata);
+        console.log(JSON.parse(responseText));
 
         $(function () {
             $('#table').bootstrapTable({
