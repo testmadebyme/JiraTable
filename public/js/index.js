ce28d3f574b7cc839eb15f5a6e1c5380a9358fbe
Olga Sheremeta <ola.sheremeta@gmail.com>
2021-06-04 16:32:46 +0300

    ghhjjj

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,38 @@
 $(document).ready(function () {
-    alert();
+    alert("Hello World");
 });
+
+var $table = $('#table')
+
+$(function () {
+
+
+
+    var data = [];
+    $.get('https://bymetest.atlassian.net/rest/api/3/issuetype').done(function (response) {
+        responseData = response['auszug'];
+
+        // Here you have to flat the array
+        Object.keys(responseData).forEach(function (key) {
+
+            var value = responseData[key];
+            data.push(value);
+        })
+        $table.bootstrapTable({ data: data })
+    })
+
+
+}
+
+
+
+
+    // method: 'GET',
+    //     headers: {
+    //     'Authorization': `Basic ${Buffer.from(
+    //         'ola.sheremeta@gmail.com:xlXAC0FJWh2cnem4MJOe786A'
+    //     ).toString('base64')}`,
+    //         'Accept': 'application/json'
+    // }
+
 
